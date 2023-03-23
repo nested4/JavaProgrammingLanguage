ead32fb50ef679fdf93b0f9e8fbc731ec009d7b3
ihsan <111647146+nested4@users.noreply.github.com>
2023-03-23 23:39:51 +0300

    day 18 package added

---
+++ src/day18_CustomMethods/C01_CustomMethods.java
@@ -0,0 +1,16 @@
+package day18_CustomMethods;
+
+public class C01_CustomMethods {
+    public static void main(String[] args) {
+        System.out.println("Program  started");
+        greeting();
+        greeting();
+        greeting();
+        System.out.println("Program completed");
+    }
+
+    public static void greeting(){
+        System.out.println("What's up Guys!");
+        System.out.println("How are you doing today?");
+    }
+}
