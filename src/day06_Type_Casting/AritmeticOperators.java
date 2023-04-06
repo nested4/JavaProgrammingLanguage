7e56db99b5229cf191a0f11a830afc291ffe5f03
ihsan <111647146+nested4@users.noreply.github.com>
2023-04-07 00:24:47 +0300

    day 29 package added

---
+++ src/day06_Type_Casting/AritmeticOperators.java
@@ -0,0 +1,35 @@
+package day06_Type_Casting;
+
+public class AritmeticOperators {
+    public static void main(String[] args){
+
+    /*   //int z=x+y;
+       // int z=x-y;
+       // int z=x*y;
+      // double z=x/(double)y;
+        int x=145673451;
+        int y=9;
+        //         5.0 /2.0=2.5
+      // double t=x/(double)y;
+        int t=x%y;
+       System.out.print(t);
+
+*/
+        double sayi=3+(double)5/2*4%(12-9);
+        //           3+5.0/2*4%3
+        //           3*2.5*4%3
+        //           3+10.0%3
+        //           3+1.0
+        //            4.0
+        //             4
+        System.out.print(sayi);
+        //       3+5/2*4%3
+        //       3+2*4%3
+        //       3+8%3
+        //       3+2
+        //        5
+
+
+    }
+
+}
