bc2f51cf026ef149a8f8b758535397339276d546
ihsan <111647146+nested4@users.noreply.github.com>
2023-04-07 00:19:42 +0300

    day 29 package added

---
+++ src/day03_EscapeChar_Variables/C07_IntroduceYourself.java
@@ -0,0 +1,27 @@
+package day03_EscapeChar_Variables;
+
+public class C07_IntroduceYourself {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Guys!");
+        System.out.println("My name is: Ihsan");
+        System.out.println("I am From: Michigan");
+        System.out.println("I am currently living in: Ankara");
+        System.out.println("My favorite programming language is: JAVA");
+        System.out.println("My dream job is: QA Automation Engineer");
+
+    }
+
+}
+/*
+ Create a class named _04_IntroduceYourself and print the following information
+about you:
+        1. Hello Guys!
+        2. My name is: Name
+        3. I am -- years old.
+        4. I am From: City
+        5. I am currently living in: City
+        6. My favorite programming language is: JAVA
+        7. My dream job is: Job
+ */
