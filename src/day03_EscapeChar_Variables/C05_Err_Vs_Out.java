fe78c229a6546a6ab8e7610b66325a6de84a5a74
ihsan <111647146+nested4@users.noreply.github.com>
2023-04-07 00:19:26 +0300

    day 29 package added

--- src/day03_EscapeChar_Variables/C05_Err_Vs_Out.java
+++ src/day03_EscapeChar_Variables/C05_Err_Vs_Out.java
@@ -1,6 +1,6 @@
 package day03_EscapeChar_Variables;
 
-public class Err_Vs_Out {
+public class C05_Err_Vs_Out {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("--------------------------");
