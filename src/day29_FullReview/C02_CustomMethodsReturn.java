71e27f678e0aa1534a06be64fc950a51f87ce3b2
ihsan <111647146+nested4@users.noreply.github.com>
2023-04-06 23:47:18 +0300

    day 29 package added

--- src/day29_FullReview/C02_CustomMethodsReturn.java
+++ src/day29_FullReview/C02_CustomMethodsReturn.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class C02_CustomMethodsReturn {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int num= scan.nextInt();
-        if(isEven(num)){
-            System.out.println(num+" is an Even number");
+        int num = scan.nextInt();
+        if (isEven(num)) {
+            System.out.println(num + " is an Even number");
         }
 
         System.out.println("---------------------------------------------");
@@ -17,18 +17,18 @@ public class C02_CustomMethodsReturn {
         System.out.println("printNumbers(10,20) = " + printNumbers(10, 20));
     }
 
-    public static boolean isEven(int num){
-       return num%2==0? true: false;
+    public static boolean isEven(int num) {
+        return num % 2 == 0 ? true : false;
     }
 
-    public static int ageOfPerson(int birthYear){
+    public static int ageOfPerson(int birthYear) {
         return 2023 - birthYear;
     }
 
-    public static String printNumbers(int X, int Y){
-        String result="";
-        for (int i=X+1; i<Y;i++){
-            result+=i+" ";
+    public static String printNumbers(int X, int Y) {
+        String result = "";
+        for (int i = X + 1; i < Y; i++) {
+            result += i + " ";
         }
         return result;
     }
