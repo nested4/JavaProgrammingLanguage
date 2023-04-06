2bdc9aab6544e9c4c529f4f4836759d8aafbb609
ihsan <111647146+nested4@users.noreply.github.com>
2023-04-07 00:19:07 +0300

    day 29 package added

--- src/day03_EscapeChar_Variables/C04_HaftaninGunleri.java
+++ src/day03_EscapeChar_Variables/C04_HaftaninGunleri.java
@@ -1,6 +1,6 @@
 package day03_EscapeChar_Variables;
 
-public class HaftaninGunleri {
+public class C04_HaftaninGunleri {
     public static void main(String[] args) {
         System.out.println("Pazartesi\nSali\nCarsamba\nPersembe\nCuma\n\nCumartesi\nPazar");
     }
