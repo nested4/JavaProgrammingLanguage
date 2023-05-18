fe82315216d920d3dcb572940d47694711683859
ihsan <111647146+nested4@users.noreply.github.com>
2023-05-18 23:04:23 +0300

    day57 package added

--- src/day57_JavaReview2/C02_ImportStatements.java
+++ src/day57_JavaReview2/C02_ImportStatements.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 import static java.lang.Math.PI;
 import static java.lang.Math.pow;
 import static java.util.Arrays.sort;
+
 public class C02_ImportStatements {
     public static void main(String[] args) {
-        String[] group ={"Hans","Sam","Tony","Jony","Hergel","Frank"};
+        String[] group = {"Hans", "Sam", "Tony", "Jony", "Hergel", "Frank"};
 
         Arrays.sort(group);
         System.out.println(Arrays.toString(group));
