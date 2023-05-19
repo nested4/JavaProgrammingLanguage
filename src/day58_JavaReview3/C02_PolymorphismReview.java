9f3f2a5bc730da11ad0c33b6b0efaf8a3022f106
ihsan <111647146+nested4@users.noreply.github.com>
2023-05-20 00:35:23 +0300

    day58 package added

--- src/day58_JavaReview3/C02_PolymorphismReview.java
+++ src/day58_JavaReview3/C02_PolymorphismReview.java
@@ -6,16 +6,16 @@ public class C02_PolymorphismReview {
     public static void main(String[] args) {
 
 //        Car car = new Car(); // Can not create obj from abstract classes\
-          Car car1= new Tofas();
-          Car car2= new BMW();
-          Car car3= (Car) new Togg(); // Typecasting upcasting
+        Car car1 = new Tofas();
+        Car car2 = new BMW();
+        Car car3 = (Car) new Togg(); // Typecasting upcasting
 
 //        Autopilot autopilot = new Autopilot() // an not create obj from interface
 //            Autopilot autopilot = (Autopilot) new Tofas();  // is a relation
 //        System.out.println(autopilot);
         //ClassCastException
 
-        Autopilot autopilot= new BMW();
+        Autopilot autopilot = new BMW();
         autopilot.autoPark();
 
         Car electricCar = new Togg();
@@ -23,8 +23,7 @@ public class C02_PolymorphismReview {
         electricCar.start();
         electricCar.stop();
 //        electricCar.autoPark(); // Reference type decides what to be access
-        ((Togg)electricCar).autoPark(); // Typecasting Downcasting
-
+        ((Togg) electricCar).autoPark(); // Typecasting Downcasting
 
 
     }
