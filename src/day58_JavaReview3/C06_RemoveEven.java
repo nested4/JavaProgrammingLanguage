d1d8fe08809428e19d8b8925a74c97162638c19f
ihsan <111647146+nested4@users.noreply.github.com>
2023-05-20 00:36:20 +0300

    day58 package added

--- src/day58_JavaReview3/C06_RemoveEven.java
+++ src/day58_JavaReview3/C06_RemoveEven.java
@@ -9,13 +9,13 @@ public class C06_RemoveEven {
 
     public static void main(String[] args) {
 
-        Set<Integer> set = new LinkedHashSet<>(Arrays.asList(1,2,3,4,5,6,100, 90, 80, 75, 45, 35, 25, 15, 1,2,3,4,5,6,100));
+        Set<Integer> set = new LinkedHashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 100, 90, 80, 75, 45, 35, 25, 15, 1, 2, 3, 4, 5, 6, 100));
 
 //         set.removeIf( p -> p %2 ==0);
 
-        Iterator<Integer> it= set.iterator();
-        while(it.hasNext()){
-            if(it.next() %2 ==0){
+        Iterator<Integer> it = set.iterator();
+        while (it.hasNext()) {
+            if (it.next() % 2 == 0) {
                 it.remove();
             }
         }
